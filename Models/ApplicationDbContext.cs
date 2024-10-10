9a809b845afa9bc37e656bdcc6ba26e3f057b5dc
Truong Nguyen <vonguyen.0407@gmail.com>
2024-10-10 09:00:03 +0700

    config for connect database, install package and create applicationdbcontex in model

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace doan1_v1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+        {
+        }
+    }
+
+}

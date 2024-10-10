7b84d4b2eff430df5b831089036d07a891100ecd
Truong Nguyen <vonguyen.0407@gmail.com>
2024-10-10 08:16:19 +0700

    start project 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace doan1_v1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

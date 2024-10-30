89e438ff9ef3cd95b75ed434c3dc8fb428372d85
Truong Nguyen <vonguyen.0407@gmail.com>
2024-10-30 15:23:10 +0700

    add signup and signin form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -45,6 +45,14 @@ namespace doan1_v1.Controllers
         {
             return View();
         }
+		public IActionResult SignIn()
+		{
+			return View();
+		}
+        public IActionResult SignUp()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
